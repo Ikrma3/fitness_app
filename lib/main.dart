c7ef224c39bb35edc68ddfa6f8adb49aa4e8c9a5
Ikrma <ikrmaiftikhar3@gmail.com>
2024-07-08 17:07:54 +0500

    6th

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:myfitness/screens/CNMViewScreen.dart';
 import 'package:myfitness/screens/breakFastScreen.dart';
+import 'package:myfitness/screens/changePassword.dart';
 import 'package:myfitness/screens/diaryScreen.dart';
 import 'package:myfitness/screens/goalScreen.dart';
 import 'package:myfitness/screens/height.dart';
 import 'package:myfitness/screens/home.dart';
 import 'package:myfitness/screens/myPlans.dart';
+import 'package:myfitness/screens/notifications.dart';
 import 'package:myfitness/screens/onBoard.dart';
 import 'package:myfitness/screens/plansScreen.dart';
+import 'package:myfitness/screens/profile.dart';
 import 'package:myfitness/screens/questionsScreen.dart';
+import 'package:myfitness/screens/reminder.dart';
 import 'package:myfitness/screens/settings.dart';
 import 'package:myfitness/screens/signin.dart';
 import 'package:myfitness/screens/signup.dart';
@@ -33,7 +37,7 @@ class MyApp extends StatelessWidget {
             theme: ThemeData.light(), // Default light theme
             darkTheme: ThemeData.dark(), // Default dark theme
             themeMode: ThemeMode.system,
-            home: GoalScreen());
+            home: PasswordScreen());
       },
       // Use the system theme
     );
